46e038cc65cf14fcef4bc86042b9ffa8e7208ee1
Lathaa-Sree <lathaa.sree.cer20@itbhu.ac.in>
2021-01-26 18:50:07 +0530

    Assignment1
    completed

---
+++ Week1/hai/work.py
@@ -0,0 +1,104 @@
+import math
+import numpy as np
+
+def demo(x):
+    '''
+    This is a demo function
+    Where in you just return square of the number
+    args:
+        x (int)
+    returns:
+        x*x (int)
+    '''
+
+
+    return (x*x)
+
+def is_palindrome(string):
+    '''
+    This function returns True if the given string is
+    a Palindrome
+    args:
+        string (str)
+    returns:
+        flag (bool)
+    '''
+
+    temp=""
+    for i in string:
+        temp=i+temp
+    if (temp==string):
+        return True
+    return False
+
+def sqrt_of_numbers(num):
+    '''
+    This function returns the magnitude of the square root of the number
+    args:
+        num (int) Need not be positive
+    returns:
+        sqroot (float)
+    '''
+    if (num>=0):
+        return math.sqrt(num)
+    r=str(math.sqrt((abs(num))))+' i'
+    return r
+
+def Maximum(arr):
+    '''
+    This function returns first maximum and the second minimum
+    number in the array
+    args:
+        arr (list)
+    returns:
+        Max1, Max2 (int, int)
+    '''
+    arr.sort(reverse=True)
+
+    return [arr[0], arr[1]]
+
+def even_sort(arr):
+    '''
+    This function sorts the array giving higher preference to even numbers
+    args:
+        arr (list)
+    returns:
+        sort_arr (list)
+    ex:
+        arr = [15, 2, 6, 88, 7]
+        ## then
+        sort_arr = [2, 6, 88 ,7 ,15]
+        ## This is any even number is smaller than any odd number
+    '''
+    even = []
+    odd = []
+    for i in arr:
+        if (i % 2 == 0):
+            even.append(i)
+        else:
+            odd.append(i)
+    even.sort()
+    odd.sort()
+    even.extend(odd)
+    return even
+
+
+def eqn_solver(A, B, C):
+    '''
+    This function solves a two variable system
+    i.e.,
+        A = [ 1, 2 ]
+        B = [ 3, 4 ]
+        C = [ 5, 6 ]
+        then it means
+        1x + 3y = 5
+        2x + 4y = 6
+        Hence you are required to find x, y for such a linear system
+    args:
+        A, B, C (list, list, list) representing coefficients in the equation
+    returns:
+        x, y (float, float)
+    '''
+
+    ## Code Here
+    return None
